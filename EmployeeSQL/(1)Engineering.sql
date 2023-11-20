3e2f56ab597525b62d8d445c256f894a7d7e3161
Zeal Akula <AkulaZ@outlook.com>
2023-11-20 16:52:22 -0600

    Resubmission

--- EmployeeSQL/(1)Engineering.sql
+++ EmployeeSQL/(1)Engineering.sql
@@ -1,7 +1,7 @@
 -- Titles 
 DROP TABLE IF EXISTS titles CASCADE;
 CREATE TABLE Titles(
-	title_id VARCHAR PRIMARY KEY,
+	title_id VARCHAR (5) PRIMARY KEY,
 	title VARCHAR
 );
 
@@ -9,26 +9,26 @@ CREATE TABLE Titles(
 DROP TABLE IF EXISTS employees CASCADE;
 CREATE TABLE Employees(
 	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR,
+	emp_title_id VARCHAR (5),
 	birth_date DATE,
 	first_name VARCHAR,
 	last_name VARCHAR,
-	sex VARCHAR,
-	hire_date VARCHAR,
+	sex VARCHAR (1),
+	hire_date VARCHAR (9),
 	FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
 );
 
 -- Departments
 DROP TABLE IF EXISTS departments;
 CREATE TABLE Departments(
-	dept_no VARCHAR PRIMARY KEY,
+	dept_no VARCHAR (4) PRIMARY KEY,
 	dept_name VARCHAR
 );
 
 -- Department Manager
 DROP TABLE IF EXISTS deptMgr CASCADE;
 CREATE TABLE deptMgr(
-	dept_no VARCHAR,
+	dept_no VARCHAR (4),
 	emp_no INT,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -39,7 +39,7 @@ CREATE TABLE deptMgr(
 DROP TABLE IF EXISTS deptEmp CASCADE;
 CREATE TABLE deptEmp(
 	emp_no INT,
-	dept_no VARCHAR,
+	dept_no VARCHAR (4),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
